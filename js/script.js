0d01a4e461aa8c42a0897b9acc4bbfab7205e820
hdongqt <hdongqt@gmail.com>
2025-07-30 11:56:08 +0700

    init project

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let menuOpen = false;
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  menuOpen = true;
+  headerEl.classList.toggle("nav-open");
+});
+
+window.addEventListener("keydown", function (e) {
+  if (menuOpen && e.key === "Escape") {
+    menuOpen = false;
+    headerEl.classList.remove("nav-open");
+  }
+});
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
